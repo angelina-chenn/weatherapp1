2cbf8834ed7b37941232ec2d2dcaa4f9659349b1
froootloops <asc337@cornell.edu>
2023-07-26 08:43:51 -0500

    Initial Commit

---
+++ weather1/Views/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  weather1
+//
+//  Created by Angelina Chen on 7/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var locationManager = LocationManager()
+    var weatherManager = WeatherManager()
+    @State var weather: ResponseBody?
+    
+    var body: some View {
+        VStack {
+            
+            if let location = locationManager.location {
+                if let weather = weather {
+                WeatherView(weather:weather)
+                } else {
+                    LoadingView()
+                        .task {
+                            do{
+                                weather = try await weatherManager.getCurrentWeather(latitude: location.latitude, longitude: location.longitude)
+                            } catch{
+                                print("Error getting weather: \(error)")
+                            }
+                        }
+                }
+            } else {
+                if locationManager.isLoading {
+                    LoadingView()
+                } else {
+                    WelcomeView()
+                        .environmentObject(locationManager)
+                }
+            }
+            
+        }
+        .background(Color(hue: 0.632, saturation: 0.562, brightness: 0.678))
+        .preferredColorScheme(.dark)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ weather1/weather1App.swift
@@ -0,0 +1,17 @@
+//
+//  weather1App.swift
+//  weather1
+//
+//  Created by Angelina Chen on 7/25/23.
+//
+
+import SwiftUI
+
+@main
+struct weather1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
